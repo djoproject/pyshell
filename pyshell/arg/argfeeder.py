903ad78d18e9549d246994af905f982c93aa4c33
djo <pyshell@djoproject.net>
2014-01-07 17:13:43 +0100

    test phase of argfeeder in progress, almost finished

--- pyshell/arg/argfeeder.py
+++ pyshell/arg/argfeeder.py
@@ -20,16 +20,27 @@ class ArgsChecker():
     def usage(self):
         pass #XXX to override
 
-class ArgFeeder2(ArgsChecker):
+#TODO
+    #self.argTypeList devrait idealement etre un dictionnaire ordonnee
+        #mais cela implique que la definition du dico ordonnée soit utilisée en dehors
+        #perte en genericite
+            #mais ici, c'est un peu degeu, on a une liste qui contient des paires key/value
+                #en soi argfeeder est initialisé dans un decorateur, ça ne passe pas dans le code utilisateur
+
+    #si len(self.argTypeList) == 0, on ne renvoit rien
+        #si pas de parametre a binder, pas de parametre a retourner, meme en cas de data, pas notre prblm
+            #semble coherent
+
+class ArgFeeder(ArgsChecker):
 
     #
     # @param argTypeList, une liste de tuple (Argname,ArgChecker) 
     #
-    def __init__(self,argTypeList=None):
-        if argTypeList == None:
-            self.argTypeList = []
-        else:
-            self.argTypeList = argTypeList
+    def __init__(self,argTypeList):
+        if argTypeList == None or not isinstance(argTypeList,list): 
+            raise argInitializationException("(ArgFeeder) argTypeList must be a valid list")
+        
+        self.argTypeList = argTypeList
         
     #
     # @argsList, une liste de string
@@ -42,12 +53,11 @@ class ArgFeeder2(ArgsChecker):
         argCheckerIndex = 0
         dataIndex = 0
         for (name,checker) in self.argTypeList:
-            
             #is there a minimum limit
             if checker.minimumSize != None:
                 #is there at least minimumSize item in the data stream?
                 if len(argsList[dataIndex:]) < checker.minimumSize:
-                    #end of stream ?
+                    #no more string token, end of stream ?
                     if len(argsList[dataIndex:]) == 0:
                         #we will check if there is some default value
                         break 
@@ -57,11 +67,11 @@ class ArgFeeder2(ArgsChecker):
             
             #is there a maximum limit?
             if checker.maximumSize == None:
-                #No max limit, it consume all remaining data
+                #No max limit, it consumes all remaining data
                 ret[name] = checker.getValue(argsList[dataIndex:],dataIndex)
-                dataIndex = len(argsList)
+                dataIndex = len(argsList) #will not stop the loop but will notify that every data has been consumed
             else:
-                #checker only need one item?
+                #special case: checker only need one item? (most common case)
                 if checker.minimumSize != None and checker.minimumSize == checker.maximumSize == 1:
                     value = argsList[dataIndex:(dataIndex+checker.maximumSize)][0]
                 else:
@@ -72,7 +82,6 @@ class ArgFeeder2(ArgsChecker):
                     
             argCheckerIndex += 1
         
-        
         # MORE THAN THE LAST ARG CHECKER HAVEN'T BEEN CONSUMED YET
         for i in range(argCheckerIndex,len(self.argTypeList)):
             (name,checker) = self.argTypeList[i]
@@ -81,6 +90,8 @@ class ArgFeeder2(ArgsChecker):
                 ret[name] = checker.getDefaultValue()
             else:
                 raise argException("(ArgFeeder) some arguments aren't bounded, missing data : <"+name+">")
+
+        #don't care about unused data in argsList, if every parameter are binded, we are happy :)
 
         return ret
         
@@ -99,7 +110,9 @@ class ArgFeeder2(ArgsChecker):
                 firstMandatory = True
             
             ret += name+":"+checker.getUsage()+" "
-            
+        
+        ret = ret.strip()
+        
         if firstMandatory:
             ret += "]"
         
--- pyshell/test/argfeederTest.py
+++ pyshell/test/argfeederTest.py
@@ -3,16 +3,81 @@
 
 import unittest
 from pyshell.arg.argchecker import *
+from pyshell.arg.argfeeder import *
 
 class ArgCheckerTest(unittest.TestCase):
     def test_init(self):
-        pass
+        self.assertRaises(argInitializationException, ArgFeeder, None)
+        self.assertRaises(argInitializationException, ArgFeeder, "toto")
+        self.assertRaises(argInitializationException, ArgFeeder, 52)
+        
+        self.assertTrue(ArgFeeder([]) != None)
         
     def test_checkArgs(self):
+        #test a list of 1 arg without limit size
+        af = ArgFeeder([("toto1",ArgChecker(None, None),)])
+        self.assertTrue(af.checkArgs(["1", "2", "3"])["toto1"] == ["1", "2", "3"])
+
+        #test a list of 2 arg, the first one without limit size and the second one need at least one token but without default value
+        af = ArgFeeder([("toto1",ArgChecker(None, None),), ("toto2",ArgChecker(),)])
+        self.assertRaises(argException,af.checkArgs, ["1", "2", "3"])
+        
+        #test a list of 2 arg, the first one without limit size and the second one need at least one token but has a default value
+        ac = ArgChecker()
+        ac.setDefaultValue("plop")
+        af = ArgFeeder([("toto1",ArgChecker(None, None),), ("toto2",ac,)])
+        r = af.checkArgs(["1", "2", "3"])
+        self.assertTrue(r["toto1"] == ["1", "2", "3"])
+        self.assertTrue(r["toto2"] == "plop")
+        
+        #test a list of 3 arg, the first one without limit size and the two last need at least one token
+        af = ArgFeeder([("toto1",ArgChecker(None, None),), ("toto2",ArgChecker(),), ("toto3",ArgChecker(),)])
+        self.assertRaises(argException,af.checkArgs, ["1", "2", "3"])
+
+        #test a list of 1 arg with a maximum limit size
+        af = ArgFeeder([("toto1",ArgChecker(None, 5),),])
+        self.assertTrue(argException,af.checkArgs(["1", "2", "3"]) == ["1", "2", "3"])
+        
+        af = ArgFeeder([("toto1",ArgChecker(None, 2),),])
+        self.assertTrue(argException,af.checkArgs(["1", "2", "3"]) == ["1", "2"])
+        
+        #TODO test a list of 2 arg the first one with a maximum limit size and the second one without
+        #TODO make the two previous test with arg of fixed size 1, and with variable size
+    
+        #TODO test also with too many string token
+            #not possible when there is a checker without limit
+    
         pass
         
     def test_usage(self):
-        pass
+        f = ArgFeeder([("toto1",ArgChecker(),)])
+        self.assertTrue(f.usage() == "toto1:<any>")
+    
+        #test with only mandatory arg
+        f = ArgFeeder([("toto1",ArgChecker(),) , ("toto2",ArgChecker(),), ("toto3",ArgChecker(),)])
+        self.assertTrue(f.usage() == "toto1:<any> toto2:<any> toto3:<any>")
+        
+        #test with only not mandatory arg
+        a = ArgChecker()
+        a.setDefaultValue("plop")
+        f = ArgFeeder([("toto1",a,) , ("toto2",a,), ("toto3",a,)])
+        self.assertTrue(f.usage() == "[toto1:<any> toto2:<any> toto3:<any>]")
+        
+        f = ArgFeeder([("toto1",a,)])
+        self.assertTrue(f.usage() == "[toto1:<any>]")
+        
+        #test with both mandatory and not mandatory arg
+        f = ArgFeeder([("toto1",ArgChecker(),) , ("toto2",a,), ("toto3",a,)])
+        self.assertTrue(f.usage() == "toto1:<any> [toto2:<any> toto3:<any>]")
+        
+        f = ArgFeeder([("toto1",ArgChecker(),), ("toto1b",ArgChecker(),) , ("toto2",a,), ("toto3",a,)])
+        self.assertTrue(f.usage() == "toto1:<any> toto1b:<any> [toto2:<any> toto3:<any>]")
+        
+        f = ArgFeeder([("toto1",ArgChecker(),) , ("toto2",a,)])
+        self.assertTrue(f.usage() == "toto1:<any> [toto2:<any>]")
+        
+        f = ArgFeeder([("toto1",ArgChecker(),), ("toto1b",ArgChecker(),) , ("toto2",a,)])
+        self.assertTrue(f.usage() == "toto1:<any> toto1b:<any> [toto2:<any>]")
                 
 if __name__ == '__main__':
     unittest.main()
