6a132e4b64f05f2b0f6caabd98cfc3575cb32ecc
djo <me@djoproject.net>
2014-05-09 23:00:59 +0200

    some work in loader

--- pyshell/command/loader.py
+++ pyshell/command/loader.py
@@ -1,23 +1,51 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-class loader(object):
+from command import MultiCommand
+from exception import LoadException
+
+_raiseIfInvalidKeyList(keyList, methName):
+    if not hasattr(keyList,"__iter__"):
+        raise LoadException("(Loader) "+methName+", keyList is not iterable")
+
+    for key in keyList:
+        if type(key) != str and type(key) != unicode:
+            raise LoadException("(Loader) "+methName+", only string or unicode key are allowed")
+
+        if len(key) == 0:
+            raise LoadException("(Loader) "+methName+", empty key is not allowed")
+
+class Loader(object):
     def __init__(self, prefix=()):
         self.prefix  = prefix
         self.cmdDict = {}
 
-    def addCommand(self, keyList, cmd):
-        #TODO check cmd and keylist
+    def addInstanciatedCommand(self, keyList, cmd):
+        #must be a multiCmd
+        if not isinstance(cmd, MultiCommand):
+            raise LoadException("(Loader) addInstanciatedCommand, cmd must be an instance of MultiCommand")
+
+        #check cmd and keylist
+        _raiseIfInvalidKeyList(keyList, "addInstanciatedCommand")
     
         self.cmdDict[" ".join(keyList)] = (keyList, cmd,)
     
-    def addCommand(self, keyList, pre=None,pro=None,post=None):
-        pass #TODO create and add a singlecommand
-        
-    def createMultiCommand(self, keyList):
-        pass #TODO return an empty multicommand
+    def addCommand(self, keyList, pre=None,pro=None,post=None, showInHelp=True):
+        #check cmd and keylist
+        _raiseIfInvalidKeyList(keyList, "addCommand")
 
-    #TODO create more command adder
+        name = " ".join(keyList)
+        self.cmdDict[name] = (keyList,UniCommand(name, pre,pro,post, showInHelp),)
+        
+    def createMultiCommand(self, keyList, showInHelp=True):
+        #check cmd and keylist
+        _raiseIfInvalidKeyList(keyList, "createMultiCommand")
+
+        name = " ".join(keyList)
+        cmd = MultiCommand(name, showInHelp)
+        self.cmdDict[name] = (keyList, cmd,)
+
+        return cmd
     
     def _load(self, mltries):
         for k,v in self.cmdDict:
