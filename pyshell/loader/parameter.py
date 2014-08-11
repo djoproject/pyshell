770d3cf877c67a34ed5d06b963dda61a9aca9910
djo938 <me@djoproject.net>
2014-08-11 17:27:07 +0200

    still refactoring on parameter loader

--- pyshell/loader/parameter.py
+++ pyshell/loader/parameter.py
@@ -16,27 +16,34 @@
 #You should have received a copy of the GNU General Public License
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+#TODO
+    #quid:
+        #si on ajoute une valeur qui existe deja
+        #lorsqu'on unload, on ne devrait pas la retirer
+        #impliquerai d'avoir un etat du loadeur par rapport a son chargement
+
 from utils import getAndInitCallerModule, AbstractLoader
 from pyshell.utils.parameter import EnvironmentParameter, ContextParameter
+from exception import LoadException
 
 def _local_getAndInitCallerModule(subLoaderName = None)
     return getAndInitCallerModule(ParamaterLoader.__module__+"."+ParamaterLoader.__name__,ParamaterLoader, 3, subLoaderName)
 
-def registerAddValueToContext(contextKey, value, subLoaderName = None):
+def registerAddValuesToContext(contextKey, value, subLoaderName = None):
     #test key
     if type(contextKey) != str and type(contextKey) != unicode:
-        raise LoadException("(Loader) registerSetParameterValue, only string or unicode key are allowed")
+        raise LoadException("(Loader) registerAddValueToContext, only string or unicode key are allowed")
 
     loader = _local_getAndInitCallerModule(subLoaderName)
-    loader.valueToAddToContext.append( (contextKey, value, ) )
+    loader.valueToAddTo.append( (contextKey, value,CONTEXT_NAME, ) )
     
-def registerAddValueToEnvironment(envKey, value, subLoaderName = None):
+def registerAddValuesToEnvironment(envKey, value, subLoaderName = None):
     #test key
     if type(envKey) != str and type(envKey) != unicode:
         raise LoadException("(Loader) registerAddValueToEnvironment, only string or unicode key are allowed")
 
     loader = _local_getAndInitCallerModule(subLoaderName)
-    loader.valueToAddToEnv.append( (envKey, value,) )
+    loader.valueToAddTo.append( (envKey, value, ENVIRONMENT_NAME,) )
 
 def registerSetEnvironment(envKey, env, noErrorIfKeyExist = False, override = False, subLoaderName = None):
     ##test key
@@ -48,7 +55,7 @@ def registerSetEnvironment(envKey, env, noErrorIfKeyExist = False, override = Fa
         raise LoadException("(Loader) registerSetEnvironmentValue, env must be an instance of EnvironmentParameter")
 
     loader = _local_getAndInitCallerModule(subLoaderName)
-    loader.env.append( (envKey, env, noErrorIfKeyExist, override) )
+    loader.valueToSet.append( (envKey, env, noErrorIfKeyExist, override,ENVIRONMENT_NAME,) )
     
 def registerSetContext(contextKey, context, noErrorIfKeyExist = False, override = False, subLoaderName = None):
     ##test key
@@ -60,16 +67,17 @@ def registerSetContext(contextKey, context, noErrorIfKeyExist = False, override 
         raise LoadException("(Loader) registerSetEnvironmentValue, env must be an instance of EnvironmentParameter")
 
     loader = _local_getAndInitCallerModule(subLoaderName)
-    loader.context.append( (contextKey, context, noErrorIfKeyExist, override) )
+    loader.valueToSet.append( (contextKey, context, noErrorIfKeyExist, override,CONTEXT_NAME,) )
 
 
 
 #TODO for the following one, value should be an instance of parameter
     #so an instance ov env ? or just a string
+        #just a string and convert it to env in the register
 
 #TODO register properties (?)
     #need the end of brainstorming in addon.parameter
-def registerSetParameterValue(paramKey, value, noErrorIfKeyExist = False, override = False, parent = None, subLoaderName = None):
+"""def registerSetParameterValue(paramKey, value, noErrorIfKeyExist = False, override = False, parent = None, subLoaderName = None):
     #test key
     if type(paramKey) != str and type(paramKey) != unicode:
         raise LoadException("(Loader) registerSetParameterValue, only string or unicode key are allowed")
@@ -85,148 +93,92 @@ def registerSetParameterValue(paramKey, value, noErrorIfKeyExist = False, overri
 
     #append
     loader = _local_getAndInitCallerModule(subLoaderName)
-    loader.params.append( (paramKey, value, noErrorIfKeyExist, override, parent) )
+    loader.params.append( (paramKey, value, noErrorIfKeyExist, override, parent) )"""
 
 
     
 class ParamaterLoader(AbstractLoader):
     def __init__(self, prefix=()):
-        self.valueToAddToContext = [] #TODO load/unload
-        self.valueToAddToEnv     = [] #TODO load/unload
-    
-        self.context    = [] #TODO refactor load/unload
-        self.env        = [] #TODO refactor load/unload
+        self.valueToAddTo = [] #TODO unload
+        self.valueToSet   = [] #TODO unload
         
         #self.params     = [] #TODO ??? wait
         
+    def _addValueTo(self, parameterManager, keyName, valueToAdd, parentName, listOfExceptions):
+        if not parameterManager.hasParameter(contextKey,CONTEXT_NAME):
+            listOfExceptions.addException(LoadException("(ParamaterLoader) addValueTo, fail to add value <"+str(valueToAdd)+"> to "+str(parentName)+" <"+str(keyName)+">: unknow key name"))
+            return
+        
+        envObject = parameterManager.getParameter(contextKey, parentName)
+
+        if not isinstance(envObject.typ, listArgChecker):
+            listOfExceptions.addException(LoadException("(ParamaterLoader) addValueTo, fail to add value <"+str(valueToAdd)+"> to "+str(parentName)+" <"+str(keyName)+">: not a list parameter"))
+            return
+
+        #value must be a list
+        if not hasattr(value, "__iter__"):
+            value = (value,)
+
+        #object is readonly ?
+        if envObject.isReadOnly():
+            listOfExceptions.addException(LoadException("(ParamaterLoader) addValueTo, fail to add value <"+str(valueToAdd)+"> to "+str(parentName)+" <"+str(keyName)+">: object is readonly"))
+        
+        #check values
+        ValuesToAdd = []
+        for v in value:
+            if v in context.value:
+                listOfExceptions.addException(LoadException("(ParamaterLoader) addValueTo, fail to add value <"+str(v)+"> to "+str(parentName)+" <"+str(keyName)+">: value already exists"))
+                continue
+
+            #check value validity
+            try:
+                context.typ.checker.getValue(v,None,"Context "+contextKey)
+            except argException as argE:
+                listOfExceptions.addException(LoadException("(ParamaterLoader) addValueTo, fail to add value <"+str(valueToAdd)+"> to "+str(parentName)+" <"+str(keyName)+">: invalid value type, "+str(argE)))
+                continue
+                
+            ValuesToAdd.append(v)
+        
+        oldValues = envobject.getValue()[:]
+        oldValues.extend(ValuesToAdd)
+            
+        try:
+            envobject.setValue(oldValues)
+        except argException as argE:
+            listOfExceptions.addException(LoadException("(ParamaterLoader) addValueTo, fail to add value <"+str(v)+"> to "+str(parentName)+" <"+str(keyName)+">: invalid values, "+str(argE)))
+        except ParameterException as pe:
+            listOfExceptions.addException(LoadException("(ParamaterLoader) addValueTo, fail to add value <"+str(v)+"> to "+str(parentName)+" <"+str(keyName)+">: "+str(pe)))
+    
+    def _setValueTo(self, parameterManager, keyName, value, noErrorIfKeyExist, override, parentName, listOfExceptions):
+        if parameterManager.hasParameter(keyName, ENVIRONMENT_NAME) and not override:
+            if not noErrorIfKeyExist:
+                listOfExceptions.addException(LoadException("(ParamaterLoader) setValueTo, fail to set "+str(parentName)+" value with key <"+str(keyName)+">: key already exists"))
+            
+            return
+
+        try:
+            parameterManager.setParameter(keyName, value,parentName)
+        except ParameterException as pe:
+            listOfExceptions.addException(LoadException("(ParamaterLoader) setValueTo, fail to set "+str(parentName)+" value with key <"+str(keyName)+">: "+str(pe)))
+    
     def load(self, parameterManager = None):
-        #TODO refactor
-
-        #TODO chaque appel a parameterManager peut déclencher une exception ParameterException, les catcher !!!
-            #les appels au sous object aussi, context, env, ...
-
-        #TODO does it still work ?
-
         if parameterManager == None:
             return
 
-        errorList = []
+        exceptions = ListOfException()
+        #errorList = []
 
-        ## ADD VALUE TO CONTEXT ##
-        for contextKey, value, typ in self.context:
-            if parameterManager.hasParameter(contextKey,CONTEXT_NAME):
-                context = parameterManager.getParameter(contextKey, CONTEXT_NAME)
+        ## ADD VALUE TO CONTEXT/ENV ##
+        for contextKey, value, parent in self.valueToAddTo:
+            self._addValueTo(parameterManager, contextKey, value, parent, exceptions)
 
-                #value must be a list in context
-                if not hasattr(value, "__iter__"):
-                    value = (value,)
+        for key, instance, noErrorIfKeyExist, override,ENVIRONMENT_NAME in self.valueToSet:
+            self._setValueTo(parameterManager, key, instance, noErrorIfKeyExist, override,ENVIRONMENT_NAME, exceptions)
 
-                for v in value:
-                    if v not in context.value:
-                        try:
-                            context.value.append( context.typ.checker.getValue(v,None,"Context "+contextKey) )
-                        except argException as argE:
-                            errorList.append("fail to add value <"+str(v)+"> in context <"+contextKey+"> beacause: "+str(argE))
-            else:
-                if typ == None:
-                    print("the context <"+contextKey+"> does not exist and no type defined to create it")
-                    continue
+######################
 
-                if not hasattr(value, "__iter__"):
-                    value = (value,)
-
-                try:
-                    parameterManager.setParameter(contextKey, ContextParameter(value, typ), CONTEXT_NAME)
-                except argException as argE:
-                    errorList.append("fail to create context <"+contextKey+">, because invalid value: "+str(argE))
-                except ParameterException as ex:
-                    errorList.append("fail to create context <"+contextKey+">: "+str(ex))
-
-        ## ADD VALUE TO ENV ##
-        for envKey, value, typ in self.env:
-            if parameterManager.hasParameter(envKey, ENVIRONMENT_NAME):
-                envobject = parameterManager.getParameter(envKey, ENVIRONMENT_NAME)
-
-                if not isinstance(envobject.typ, listArgChecker):
-                    errorList.append("fail to add environment value on <"+envKey+">, because the existing one is not a list environment")
-                    continue
-
-                oldValue = envobject.getValue()[:]
-                if not hasattr(value, "__iter__"):
-                    oldValue.append(value)
-                else:
-                    oldValue.extend(value)
-
-                try:
-                    envobject.setValue(oldValue)
-                except argException as argE:
-                    errorList.append("fail to add new items to environment <"+contextKey+">, because invalid value: "+str(argE))
-                except ParameterException as pe:
-                    errorList.append("fail to add new items to environment <"+contextKey+">: "+str(pe))
-            else:
-                if typ == None:
-                    errorList.append("fail to add new items to environment <"+envKey+">, because the environment does not exist and no type is defined")
-                    continue
-
-                if not hasattr(value, "__iter__"):
-                    value = (value,)
-
-                if not isinstance(typ, listArgChecker):
-                    typ = listArgChecker(typ)
-
-                try:
-                    parameterManager.setParameter(envKey, EnvironmentParameter(value, typ), ENVIRONMENT_NAME)
-                except argException as argE:
-                    errorList.append("fail to create new environment <"+envKey+">, because invalid value: "+str(argE))
-                except ParameterException as pe:
-                    errorList.append("fail to create new environment <"+envKey+">: "+str(pe))
-
-        ## SET ENV ##
-        for envKey, value, typ, noErrorIfKeyExist, override in self.env_set:
-            if parameterManager.hasParameter(envKey, ENVIRONMENT_NAME):
-                if not override:
-                    continue
-
-                envobject = parameterManager.getParameter(envKey, ENVIRONMENT_NAME)
-
-                if not hasattr(value, "__iter__") and isinstance(envobject.typ, listArgChecker):
-                    value = (value, )
-
-                if hasattr(value, "__iter__") and not isinstance(envobject.typ, listArgChecker):
-                    value = value[0]
-
-                try:
-                    envobject.setValue(value)
-                except argException as argE:
-                    errorList.append("fail to set value to environment <"+envKey+">, because invalid value: "+str(argE))
-                except ParameterException as pe:
-                    errorList.append("fail to set value to environment <"+envKey+">: "+str(pe))
-
-            else:
-                #TODO this statement looks weird...
-                    #try to understand what does it need to do
-                    #then compare with the existing code
-
-                if typ == None:
-                    if not noErrorIfKeyExist:
-                        print("fail to add environment value on <"+envKey+">, because the environment does not exist and no type is defined")
-
-                    continue
-
-                if not hasattr(value, "__iter__") and isinstance(typ, listArgChecker):
-                    value = (value, )
-
-                if hasattr(value, "__iter__") and not isinstance(typ, listArgChecker):
-                    typ = listArgChecker(typ)
-
-                try:
-                    parameterManager.setParameter(paramKey, EnvironmentParameter(value, typ),ENVIRONMENT_NAME)
-                except argException as argE:
-                    errorList.append("fail to create new environment <"+envKey+">, because invalid value: "+str(argE))
-                except ParameterException as pe:
-                    errorList.append("fail to create new environment <"+envKey+">: "+str(pe))
-
-        ## SET PARAMS ##
+        #TODO
+        """## SET PARAMS ##
         for paramKey, value, noErrorIfKeyExist, override, parent in self.params:
             if parameterManager.hasParameter(paramKey,parent):
                 if override:
@@ -235,17 +187,16 @@ class ParamaterLoader(AbstractLoader):
                     print("fail to create parameter <"+paramKey+">, already exists and override not allowed")
 
             else:
-                parameterManager.setParameter(paramKey, EnvironmentParameter(value),parent)
+                parameterManager.setParameter(paramKey, EnvironmentParameter(value),parent)"""
 
         #print error list
-        for error in errorList:
-            print(error)
+        if exceptions.isThrowable():
+            raise exceptions
 
     def unload(self, mltries):
-        pass #TODO
+        #TODO remove values added
         
-    def reload(self, mltries):
-        self.unload(mltries)
-        self.load(mltries)
-        
+        #TODO remove object set
+    
+        pass #TODO        
 
