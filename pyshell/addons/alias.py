1bd6bac3c98d837a66c1d9d4da47fcb2acfeb714
djo938 <me@djoproject.net>
2014-09-01 17:50:11 +0200

    alias brainstorming

--- pyshell/addons/alias.py
+++ pyshell/addons/alias.py
@@ -18,13 +18,32 @@
 
 from pyshell.arg.decorator import shellMethod
 
-#TODO
-    #problm to solve
-        #name and command are both unknows length string list
+#XXX ALIAS DEFINITION
+    #an alias has a string list as name
+        #this string list is store in the command tries
+        
+    #an alias hold a list of command string and their argument (if any argument)
+        #a command could be several piped command a| b| c| ...
+    
+    #argument of command must be checked at the adding in the alias
+        #except for an alone command or the first command of a piped commands
+            #if they are allowed to receive argument from alias
+                #they can have uncomplete arg list
+            #otherwise check every args of every command
+                
+    #if argument are given to alias which command will use them ? (SEE PRBLM 4)
+        #three possibilities:
+            #all
+            #first (n first ?, list of id)
+            #none of them
+
+#TODO PRBLM 1: name and command are both unknows length string list
         #so it is difficult to known where finish the first one
         
             #Solution 1: wait for parameter implementation
+                #not intuitive
             #Solution 2: use variable
+                #not intuitive
             #Solution 3: use named alias without value at the beginning
                 #then only use add to associate a command to an alias
                     #bof, need two step to create an alias :/
@@ -34,15 +53,65 @@ from pyshell.arg.decorator import shellMethod
                 
                 #drawback
                     #alias a un seul token string
-                    #categorie parente figée
-        
-        #alias must be disabled if one of the included command has been unloaded
+                    #categorie parente figée (???)
+
+#TODO PRBLM 2: alias must be disabled if one of the included command has been unloaded
+    #disappearing of one of the used command
         #we need to know if a command is linked to an alias
         
             #Solution 1: keep a map of the alias and check at the unload
                 #the alias list ?
             #Solution 2: add an attribute to the multiCommand object
-            #Solution 3:
+                #mark the command used in an alias ?
+            #Solution 3: add an unload routine in corresponding unloader
+                #need to identify it before to add the routine
+                    #be carefull to limit the amount of process in unloader search 
+                    #to only save a limited amount of computation for an occasional unload
+                #if possible to identify unload with O(1), could be interesting 
+            
+#TODO PRBLM 3: how to store the alias ?
+    #need an efficient and simple way to store them
+        
+        #Solution 1: keep a list of alias in the system
+            #pair of (List<String>, AliasObject)
+            #store a special object in the command tree, this object will retrieve its aliasObject from the list at execution
+            
+            #PRO:
+                #easy to manage
+                    #to unload a command, just check in every alias object if the command is used
+                
+            #CON:
+                #need another additional structure to store the alias
+            
+        #Solution 2: no list in the system
+            #no more structure
+            #store a special cmd object
+                #a kind of decorated function
+                
+            #PRO
+                #no more structure
+            
+            #con
+                #hard to list existing alias, only a prlbm for list...
+                #could be interesting to reuse the help function and filter on these object
+                    #if instance of AliasObject, then print
+            
+#TODO PRBLM 4: what about piping |
+    #What happen if we add piping in alias list ?
+        #just store them like that and check the args of the command if needed
+            #if command not linked to the arg passing of the alias, check the args provided for the command
+            #if not the first command of a piped command, check the args provided
+        
+    #how to manage argument passing from alias to subcmd ?
+        #several politics possible
+        #give them to
+            #all
+            #first
+            #none of them
+            #a list of index
+            #...
+            
+        #how to efficiently manage these politics in the shell ?
             
 ## FUNCTION SECTION ##
 
