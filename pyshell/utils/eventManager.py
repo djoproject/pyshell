8752454881f673e02bb7755c7811463a505feef1
djo <me@djoproject.net>
2014-09-08 00:18:24 +0200

    some thinking about eventManager

--- pyshell/utils/eventManager.py
+++ pyshell/utils/eventManager.py
@@ -16,12 +16,59 @@
 #You should have received a copy of the GNU General Public License
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-EVENT_AT_STARTUP      = 0 #at application launch
-EVENT_AT_EXIT         = 1 #at application exit
-EVENT_AT_ADDON_LOAD   = 2 #at addon load (args=addon name)
-EVENT_AT_ADDON_UNLOAD = 3 #at addon unload (args=addon name)
+EVENT_ON_STARTUP      = "onstartup" #at application launch
+EVENT_AT_EXIT         = "atexit" #at application exit
+EVENT_AT_ADDON_LOAD   = "onaddonload" #at addon load (args=addon name)
+EVENT_AT_ADDON_UNLOAD = "onaddonunload" #at addon unload (args=addon name)
+
+import threading
 
 #XXX brainstorming
-    #event avec ou sans argument ?
+    #event avec ou sans argument ? avec
     
-    #comment sont identifier les events
+    #comment sont identifier les events ? string
+
+    #format de la commande ?
+
+    
+
+class EventManager(object):
+	def __init__(self):
+		self.events = {}
+
+	def addCommand(self, eventName, cmd):
+		#TODO check cmd
+
+		if eventName not in self.events:
+			self.events[eventName] = []
+
+		#TODO add cmd
+
+	def fireEvent(self, eventName, argList = None):
+		if eventName not in self.events:
+			pass #TODO raise
+
+		cmdList = self.events[eventName]:
+
+		#start thread
+		t = threading.Thread(None, self._fireEvent, None, (cmdList[:], argList,))
+		t.start()
+
+	def _fireEvent(self,cmdList, argList=None):
+		for cmd in cmdList:
+			self.executeCommand(cmd)
+
+	def executeCommand(self, cmd, eventArg=None):
+		#TODO get lock list
+
+		#TODO order lock
+
+		#TODO lock
+
+		#TODO execute
+
+		#TODO unlock
+
+		pass #TODO
+
+
