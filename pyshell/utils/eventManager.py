f41a38fbd076fa8436646a9b7a813d66489fb07a
djo938 <me@djoproject.net>
2014-10-09 17:55:07 +0200

    finish to define event structure, need to implement it now

--- pyshell/utils/eventManager.py
+++ pyshell/utils/eventManager.py
@@ -21,7 +21,19 @@ EVENT_AT_EXIT         = "atexit" #at application exit
 EVENT_AT_ADDON_LOAD   = "onaddonload" #at addon load (args=addon name)
 EVENT_AT_ADDON_UNLOAD = "onaddonunload" #at addon unload (args=addon name)
 
-import threading
+import threading, sys
+from pyshell.utils.exception import ListOfException
+from pyshell.utils.exception import ParameterException, ParameterLoadingException
+
+try:
+    pyrev = sys.version_info.major
+except AttributeError:
+    pyrev = sys.version_info[0]
+
+if pyrev == 2:
+    import ConfigParser 
+else:
+    import configparser as ConfigParser
 
 #TODO
     #keep track of running event and be able to kill one or all of them
@@ -42,46 +54,52 @@ import threading
     #format de la commande ? or where to convert from string to object ?
         #TYPE 1: pure texte
             #--- les erreurs de commandes inconnues se produisent lors de l'execution de l'event
+                #+++ mais possibilité de les corriger pour la prochaine execution de l'event si on load le module manquant
             #+++ possible de reconvertir en text immediatement
+            
             
         #TYPE 2: convertie en objet
             #+++ les erreurs de commandes inconnues se produisent lors de l'ajout de la commande dans un event
+                #--- si l'event n'est pas loadé ou que la ligne est retirée, cela peut modifier le comportement de l'event
             #--- pas moyen de reconvertir en texte ensuite, besoin de stocker la chaine d'origine
-            
+    
+    #what about security access to the alias ?
+        #readonly ? removable ?
+    
 class Event(object):
     def __init__(self):
-        self.cmdsAndArgsList = [] 
+        self.stringCmdList = [] 
         self.stopOnError = True #TODO do accessor
         self.argFromEventOnlyForFirstCommand = False #TODO accessor + use it 
         self.useArgFromEvent = True #TODO accessor + use it
-        
+    
     def addCommand(self, commandList, argList):
-        self.cmdList.append( (commandList, argList, ) )
+        self.stringCmdList.append( (commandList, argList, ) )
         
     def removeCommand(self, index):
         try:
-            del self.cmdList[index]
+            del self.stringCmdList[index]
         except IndexError:
             pass #do nothing
         
     def moveCommand(self,fromIndex, toIndex):
         try:
-            self.cmdList[fromIndex]
+            self.stringCmdList[fromIndex]
         except IndexError:
             pass #TODO raise
         
         #transforme to positive index if needed
         if fromIndex < 0:
-            fromIndex = len(self.cmdList) + fromIndex
+            fromIndex = len(self.stringCmdList) + fromIndex
         
         try:
-            self.cmdList[toIndex]
+            self.stringCmdList[toIndex]
         except IndexError:
             pass #TODO raise
             
         #transforme to positive index if needed
         if toIndex < 0:
-            toIndex = len(self.cmdList) + toIndex
+            toIndex = len(self.stringCmdList) + toIndex
             
         if fromIndex == toIndex:
             return
@@ -89,7 +107,7 @@ class Event(object):
         if fromIndex < toIndex:
             toIndex -= 1
             
-        self.cmdList.insert(toIndex, self.cmdList.pop(fromIndex))
+        self.stringCmdList.insert(toIndex, self.stringCmdList.pop(fromIndex))
         
     def upCommand(self,index):
         self.moveCommand(index,index-1)
@@ -100,11 +118,32 @@ class Event(object):
     def clone(self):
         pass #TODO
 
-class EventManager(object):
-	def __init__(self, filePath = None):
-		self.events = {}
-		self.filePath = filePath
+def isInt(value):
+    try:
+        i = int(value) 
+        return True, i 
+    except ValueError:
+        pass
+    
+    return False, None
+    
+def isBool(value):
+    if type(value) != str and type(value) != unicode:
+        return False, None
+        
+    if value.lower() == "true":
+        return True,True
+        
+    if value.lower() == "false":
+        return True,False
+        
+    return False,None
 
+class EventManager(object):
+    def __init__(self, filePath = None):
+        self.events = {}
+        self.filePath = filePath
+    
     def load(self):
         if self.filePath is None:
             return
@@ -117,10 +156,33 @@ class EventManager(object):
             except Exception as ex:
                 raise ParameterLoadingException("(EventManager) load, fail to read event file : "+str(ex))
         
+        errorList = ListOfException()
         for section in config.sections():
+            #TODO get or create the event
+            event = Event()
+        
             for option in config.options(section):
-                #config.get(section, option)
-                pass#TODO
+                value = config.get(section, option)
+                if option in ["stopOnError","argFromEventOnlyForFirstCommand","useArgFromEvent"] :
+                    validBool, boolValue = isBool(value)
+                    if not validBool:
+                        errorList.addException(ParameterLoadingException("") #TODO set the error message
+                        continue
+                    
+                    setattr(event,option,boolValue)
+                    
+                else:
+                    validInt, intValue = isInt(option)
+                    if not validInt:
+                        #TODO notify the error
+                    
+                        continue
+                    
+                    #TODO a new command to process
+                        #need to parse value to string token
+                        
+                    #event.stringCmdList.insert(intValue, )
+                    
         
     def save(self):
         if self.filePath is None:
@@ -131,42 +193,62 @@ class EventManager(object):
         for eventName, eventObject in self.events.items():
             config.add_section(eventName)
             
+            #TODO set event properties
+            #config.set(eventName, childName, value)
+            
+            #TODO write each command in order
             #for ...
                 #TODO generate event on text format
             
         with open(self.filePath, 'wb') as configfile:
             config.write(configfile)
     
-	def addCommand(self, eventName, cmdList, argList):
-		#TODO check cmd
+    def addCommand(self, eventName, cmdList, argList):
+        #TODO check cmd
 
-		if eventName not in self.events:
-			self.events[eventName] = Event(cmdList, argList)
+        if eventName not in self.events:
+            self.events[eventName] = Event(cmdList, argList)
 
-	def fireEvent(self, eventName, argList = None):
-		if eventName not in self.events:
-			pass #TODO raise
+    def fireEvent(self, eventName, argList = None):
+        if eventName not in self.events:
+            pass #TODO raise
 
-		event = self.events[eventName]:
+        event = self.events[eventName]
 
         #TODO clone the event and give it to the thread
             #to avoid unexpected behaviour if the event is updated after its fire
 
-		#start thread
-		t = threading.Thread(None, self._fireEvent, None, (event.cmdsAndArgsList[:], argList,))
-		t.start()
+        #start thread
+        t = threading.Thread(None, self._fireEvent, None, (event.cmdsAndArgsList[:], argList,))
+        t.start()
 
-	def _fireEvent(self, cmdsAndArgsList, eventArgList=None):
-		for cmdsAndArgs in cmdsAndArgsList:
-		    cmdObjects, argList = cmdsAndArgs
-		
-		    if eventArgList is not None:
-		        argList = argList[:]
-		        argList.append(eventArgList)
-		
-			self.executeCommand(cmdObjects, eventArgList)
+    def _fireEvent(self, cmdsAndArgsList, eventArgList=None):
+        for cmdsAndArgs in cmdsAndArgsList:
+            cmdObjects, argList = cmdsAndArgs
+        
+            if eventArgList is not None:
+                argList = argList[:]
+                argList.append(eventArgList)
+        
+            self.executeCommand(cmdObjects, eventArgList)
 
-	def executeCommand(self, cmdList, argList):
-		pass #TODO copy (or move) from executer
+    def executeCommand(self, cmdList, argList):
+        pass #TODO copy (or move) from executer
+
+if __name__ == "__main__":
+    print "plop"
+    config = ConfigParser.RawConfigParser()
+    config.read("/home/djo/development/inputTest.conf")
+    
+    for section in config.sections():
+        print section
+        for option in config.options(section):
+            print option
+            print config.get(section, option)
+
+
+        
+
+
 
 
