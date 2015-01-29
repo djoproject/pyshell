a16278d7e095b1c95de669f42fee19f716a0b8ec
djo <me@djoproject.net>
2015-01-29 23:30:39 +0100

    update util test script

--- pyshell/utils/test/test.sh
+++ pyshell/utils/test/test.sh
@@ -3,5 +3,13 @@
 export PYTHONPATH=$(pwd)/../../../:$PYTHONPATH
 echo $PYTHONPATH
 
+python -m pyshell.utils.test.aliasTest || exit
 python -m pyshell.utils.test.executingTest || exit
-
+python -m pyshell.utils.test.keystoreTest || exit
+python -m pyshell.utils.test.parameterTest || exit
+python -m pyshell.utils.test.parsingTest || exit
+python -m pyshell.utils.test.postProcessTest || exit
+python -m pyshell.utils.test.printingTest || exit
+python -m pyshell.utils.test.solvingTest || exit
+python -m pyshell.utils.test.utilsTest || exit
+python -m pyshell.utils.test.valuableTest || exit
